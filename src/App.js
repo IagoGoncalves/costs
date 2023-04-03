a42578d04f56e3cc35b98daba652a600da355ae5
IagoGoncalves <iago_375@hotmail.com>
2023-04-03 15:54:52 -0300

    terceira aula

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
     return (
         <BrowserRouter>
-            <ul>
-                <Link to="/">Home</Link>
-                <Link to="/Company">Empresa</Link>
-                <Link to="/Contact">Contato</Link>
-                <Link to="/NewProject">Novo Projeto</Link>
-            </ul>
+            <Navbar />
             <Container customClass="min-height">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/Projects" element={<Projects />} />
                     <Route path="/Company" element={<Company />} />
                     <Route path="/Contact" element={<Contact />} />
                     <Route path="/NewProject" element={<NewProject />} />
                 </Routes>
             </Container>
-            <p>Footer</p>
+            <Footer />
         </BrowserRouter>
     );
 }
